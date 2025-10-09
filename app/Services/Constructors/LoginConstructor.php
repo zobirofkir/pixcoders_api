c21a590346fefae1ba69660999af249839bad38f
csw <zobirofkir19@gmail.com>
2025-10-09 22:28:07 +0100

    create login service

--- app/Services/Constructors/LoginConstructor.php
+++ app/Services/Constructors/LoginConstructor.php
@@ -2,7 +2,13 @@
 
 namespace App\Services\Constructors;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\LoginResource;
+
 interface LoginConstructor
 {
-    //
+    /**
+     * Login The User
+     */
+    public function login(LoginRequest $request) : LoginResource;
 }
--- app/Services/Services/LoginService.php
+++ app/Services/Services/LoginService.php
@@ -2,9 +2,26 @@
 
 namespace App\Services\Services;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\LoginResource;
+use App\Models\User;
 use App\Services\Constructors\LoginConstructor;
+use Illuminate\Support\Facades\Hash;
 
 class LoginService implements LoginConstructor
 {
-    //
+    /**
+     * Login The User
+     */
+    public function login(LoginRequest $request) : LoginResource
+    {
+        $validatedData = $request->validated();
+        $user = User::where('email', $validatedData['email'])->first();
+
+        if (!$user || !Hash::check($validatedData['password'], $user->password)) {
+            abort(401);
+        }
+
+        return LoginResource::make($user);
+    }
 }
