3a1e4500aee81e4b81e0205d77343a81e4646b68
csw <zobirofkir19@gmail.com>
2025-10-09 22:24:58 +0100

    create login resource

--- app/Http/Resources/LoginResource.php
+++ app/Http/Resources/LoginResource.php
@@ -14,6 +14,10 @@ class LoginResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "email" => $this->email,
+            "accessToken" => $this->accessToken()
+        ];
     }
 }
