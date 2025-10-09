4e97d9729710d8774eec180a21f6c498216ba0f8
csw <zobirofkir19@gmail.com>
2025-10-09 22:44:33 +0100

    create auth provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
     App\Providers\LoginServiceProvider::class,
     App\Providers\PortfolioServiceProvider::class,
     App\Providers\UserServiceProvider::class,
