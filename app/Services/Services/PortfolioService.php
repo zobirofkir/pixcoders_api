4822f24db570502cf748eff3e116fec1cdf8d73b
csw <zobirofkir19@gmail.com>
2025-10-09 19:28:31 +0100

    create portfolio service

--- app/Services/Services/PortfolioService.php
+++ app/Services/Services/PortfolioService.php
@@ -2,9 +2,49 @@
 
 namespace App\Services\Services;
 
+use App\Http\Requests\PortfolioRequest;
+use App\Models\User;
 use App\Services\Constructors\PortfolioConstructor;
 
 class PortfolioService implements PortfolioConstructor
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(PortfolioRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(PortfolioRequest $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
 }
