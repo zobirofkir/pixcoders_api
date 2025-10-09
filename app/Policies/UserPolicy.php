2c3ac6e81e3b958ff4e6db3375e3104ddc6cc1f6
csw <zobirofkir19@gmail.com>
2025-10-09 22:43:08 +0100

    admin

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -28,7 +28,7 @@ class UserPolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        return $user->hasRole('admin'); 
     }
 
     /**
@@ -36,7 +36,7 @@ class UserPolicy
      */
     public function update(User $user, User $model): bool
     {
-        return false;
+        return $user->hasRole('admin'); 
     }
 
     /**
@@ -44,7 +44,7 @@ class UserPolicy
      */
     public function delete(User $user, User $model): bool
     {
-        return false;
+        return $user->hasRole('admin'); 
     }
 
     /**
