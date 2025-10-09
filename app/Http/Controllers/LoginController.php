8ff24ec32e9088b2a83e7ee80467ae46c981a55b
csw <zobirofkir19@gmail.com>
2025-10-09 22:23:43 +0100

    call the login service using facade in login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Services\Facades\LoginFacade;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    /**
+     * Login The User
+     */
+    public function login(LoginRequest $request)
+    {
+        return LoginFacade::login($request);
+    }
 }
