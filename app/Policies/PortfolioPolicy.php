1793e12881ecb33956b17c8ee255813369d2217c
csw <zobirofkir19@gmail.com>
2025-10-10 00:26:19 +0100

    create

--- app/Policies/PortfolioPolicy.php
+++ app/Policies/PortfolioPolicy.php
@@ -2,15 +2,65 @@
 
 namespace App\Policies;
 
+use App\Models\Portfolio;
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 
 class PortfolioPolicy
 {
     /**
-     * Create a new policy instance.
+     * Determine whether the user can view any models.
      */
-    public function __construct()
+    public function viewAny(User $user): bool
     {
-        //
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Portfolio $portfolio): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Portfolio $portfolio): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Portfolio $portfolio): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Portfolio $portfolio): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Portfolio $portfolio): bool
+    {
+        return false;
     }
 }
