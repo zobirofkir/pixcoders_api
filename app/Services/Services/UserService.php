f4f57ee348230e04ab0043ac1dd41096cdb9af7a
csw <zobirofkir19@gmail.com>
2025-10-09 19:22:43 +0100

    create user service

--- app/Services/Services/UserService.php
+++ app/Services/Services/UserService.php
@@ -2,9 +2,49 @@
 
 namespace App\Services\Services;
 
+use App\Http\Requests\UserRequest;
+use App\Models\User;
 use App\Services\Constructors\UserConstructor;
 
 class UserService implements UserConstructor
 {
-    //
+        /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UserRequest $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
 }
