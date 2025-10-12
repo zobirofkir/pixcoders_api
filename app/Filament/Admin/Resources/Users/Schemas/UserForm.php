f2d1cba7ec771e3efeb55ad12543c3aa72db3fcd
csw <zobirofkir19@gmail.com>
2025-10-12 08:39:37 +0100

    create

---
+++ app/Filament/Admin/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Admin\Resources\Users\Schemas;
+
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
