7f077062688032525e7817a0f2ef548e1cb61d3b
csw <zobirofkir19@gmail.com>
2025-10-12 11:37:42 +0100

    create user resource

--- app/Filament/Admin/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Admin/Resources/Users/Schemas/UserForm.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Admin\Resources\Users\Schemas;
 
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class UserForm
@@ -9,8 +11,31 @@ class UserForm
     public static function configure(Schema $schema): Schema
     {
         return $schema
-            ->components([
-                //
-            ]);
+            ->schema([
+                TextInput::make('name')
+                    ->label('Name')
+                    ->required()
+                    ->maxLength(255),
+
+                TextInput::make('email')
+                    ->label('Email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+
+                FileUpload::make('avatar')
+                    ->label('Avatar')
+                    ->image()
+                    ->directory('avatars') 
+                    ->disk('public')      
+                    ->required(false)
+                    ->maxSize(1024),
+
+                    TextInput::make('password')
+                    ->label('Password')
+                    ->password()
+                    ->required()
+                    ->maxLength(255)
+            ])->columns(1);
     }
 }
--- app/Filament/Admin/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Admin/Resources/Users/Tables/UsersTable.php
@@ -5,6 +5,8 @@ namespace App\Filament\Admin\Resources\Users\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class UsersTable
@@ -13,7 +15,26 @@ class UsersTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->label('Name')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('email')
+                    ->label('Email')
+                    ->sortable()
+                    ->searchable(),
+
+                ImageColumn::make('avatar')
+                    ->label('Avatar')
+                    ->disk('public')
+                    ->sortable()
+                    ->searchable(false),
+
+                TextColumn::make('bio')
+                    ->label('Bio')
+                    ->sortable()
+                    ->searchable(),
             ])
             ->filters([
                 //
