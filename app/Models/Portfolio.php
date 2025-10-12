28c3a93611345d002bdaf091546876b91020e580
csw <zobirofkir19@gmail.com>
2025-10-12 11:39:14 +0100

    create portfolio model

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Portfolio extends Model
 {
-    //
+    protected $fillable = [
+        "user_id",
+        "title",
+        "category", 
+        "description",
+        "image",
+        "technologies",
+        "link",
+        "featured"
+    ] ;
+
+    protected $casts = [
+        "technologies" => "array",
+        "featured" => "boolean",
+    ];
+
+    /**
+     * Relation Sheep With User
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
