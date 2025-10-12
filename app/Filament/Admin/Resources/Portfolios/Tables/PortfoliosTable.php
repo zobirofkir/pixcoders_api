117aa6d384f541c5e3e6b1abc4855f5ab46bb8ba
csw <zobirofkir19@gmail.com>
2025-10-12 11:45:19 +0100

    update

--- app/Filament/Admin/Resources/Portfolios/Tables/PortfoliosTable.php
+++ app/Filament/Admin/Resources/Portfolios/Tables/PortfoliosTable.php
@@ -2,10 +2,14 @@
 
 namespace App\Filament\Admin\Resources\Portfolios\Tables;
 
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TagsColumn;
+use Filament\Tables\Columns\IconColumn;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Tables\Table;
 
 class PortfoliosTable
 {
@@ -13,7 +17,38 @@ class PortfoliosTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('title')
+                    ->label('Title')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('category')
+                    ->label('Category')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('user.name')
+                    ->label('User')
+                    ->sortable()
+                    ->searchable(),
+
+                ImageColumn::make('image')
+                    ->label('Image')
+                    ->disk('public')
+                    ->sortable(),
+
+                TagsColumn::make('technologies')
+                    ->label('Technologies'),
+
+                TextColumn::make('link')
+                    ->label('Link')
+                    ->url(fn ($record) => $record->link)
+                    ->openUrlInNewTab(),
+
+                IconColumn::make('featured')
+                    ->label('Featured')
+                    ->boolean()
+                    ->sortable(),
             ])
             ->filters([
                 //
