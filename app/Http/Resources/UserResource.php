eb2691a80168fa08217e4720697eb473c7edade1
csw <zobirofkir19@gmail.com>
2025-10-12 13:17:42 +0100

    create user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,12 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "email" => $this->email,
+            "avatar" => asset('storage/' . $this->avatar),
+            "role" => $this->role
+        ];
     }
 }
