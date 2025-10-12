598d4707780591483a60202aaa59f33adbeb7132
csw <zobirofkir19@gmail.com>
2025-10-12 13:30:52 +0100

    add facade in blog contrller

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BlogResource;
+use App\Models\Blog;
+use App\Services\Facades\BlogFacade;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class BlogController extends Controller
 {
-    //
+    /**
+     * List all blogs
+     */
+    public function index() : AnonymousResourceCollection 
+    {
+        return BlogFacade::index();
+    }
+
+    /**
+     * Show Specific blog
+     */
+    public function show(Blog $blog) : BlogResource
+    {
+        return BlogFacade::show($blog);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\PortfolioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix("users")->group(function() {
     Route::apiResource('portfolios', PortfolioController::class);
+
+    Route::apiResource('blogs', BlogController::class);
 });
