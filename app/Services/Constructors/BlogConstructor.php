bc0bd7b2f810c2636e9657d910e1a238b1b0a665
csw <zobirofkir19@gmail.com>
2025-10-12 13:32:44 +0100

    create blog service

--- app/Services/Constructors/BlogConstructor.php
+++ app/Services/Constructors/BlogConstructor.php
@@ -2,7 +2,19 @@
 
 namespace App\Services\Constructors;
 
+use App\Http\Resources\BlogResource;
+use App\Models\Blog;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
 interface BlogConstructor
 {
-    //
+    /**
+     * List all blogs
+     */
+    public function index() : AnonymousResourceCollection;
+
+    /**
+     * Show specific blog
+     */
+    public function show(Blog $blog) : BlogResource ;
 }
--- app/Services/Services/BlogService.php
+++ app/Services/Services/BlogService.php
@@ -2,9 +2,28 @@
 
 namespace App\Services\Services;
 
+use App\Http\Resources\BlogResource;
+use App\Models\Blog;
 use App\Services\Constructors\BlogConstructor;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class BlogService implements BlogConstructor
 {
-    //
+    /**
+     * List all blogs
+     */
+    public function index() : AnonymousResourceCollection
+    {
+        return BlogResource::collection(
+            Blog::all()
+        );
+    }
+
+    /**
+     * Show specific blog
+     */
+    public function show(Blog $blog) : BlogResource
+    {
+        return BlogResource::make($blog);
+    }
 }
