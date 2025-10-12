91eb469fefcaee7026ad5acab4d90b63bcd877d1
csw <zobirofkir19@gmail.com>
2025-10-12 12:27:15 +0100

    create

---
+++ app/Enums/RolesEnum.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enums;
+
+enum RolesEnum: string
+{
+    case ADMIN = 'admin';
+    case USER = 'user';
+
+    public function label(): string
+    {
+        return match ($this) {
+            static::ADMIN => 'Administrators',
+            static::USER => 'Users',
+        };
+    }
+}
