23b77716ad10be00d2764ddc6ee3ab908f5f8b2c
csw <zobirofkir19@gmail.com>
2025-10-12 13:25:57 +0100

    create portfolio

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\PortfolioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix("users")->group(function() {
+    Route::apiResource('portfolios', PortfolioController::class);
+});
