14a47a16cf746f40a26e81780034b9158d854493
csw <zobirofkir19@gmail.com>
2025-10-12 13:25:04 +0100

    create portfolio controller

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PortfolioResource;
+use App\Models\Portfolio;
+use App\Services\Facades\PortfolioFacade;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PortfolioController extends Controller
 {
-    //
+    /**
+     * List all portfolios
+     */
+    public function index() : AnonymousResourceCollection
+    {
+        return PortfolioFacade::index();
+    }
+
+    /**
+     * Show Specific portfolio
+     */
+    public function show(Portfolio $portfolio) 
+    {
+        return PortfolioResource::make($portfolio);
+    }
 }
