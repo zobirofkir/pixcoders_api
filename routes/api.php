84fc593d45c3a6c05d2887e42a909a2d2606eeaa
csw <zobirofkir19@gmail.com>
2025-10-13 00:17:13 +0100

    create contacts route

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,27 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\PortfolioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix("users")->group(function() {
+
+    /**
+     * Portfolios Route
+     */
     Route::apiResource('portfolios', PortfolioController::class);
 
+    /**
+     * Blog Route
+     */
     Route::apiResource('blogs', BlogController::class);
+
+    /**
+     * Contact Route
+     */
+    Route::apiResource('contacts', ContactController::class);
+
+
 });
