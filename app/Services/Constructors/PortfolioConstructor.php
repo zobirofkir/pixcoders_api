93a0a6699ac68d424d34cbb8531b2b3ce17a7dba
csw <zobirofkir19@gmail.com>
2025-10-12 13:23:41 +0100

    create portfolio service and constructor

--- app/Services/Constructors/PortfolioConstructor.php
+++ app/Services/Constructors/PortfolioConstructor.php
@@ -2,7 +2,19 @@
 
 namespace App\Services\Constructors;
 
+use App\Http\Resources\PortfolioResource;
+use App\Models\Portfolio;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
 interface PortfolioConstructor
 {
-    //
+    /**
+     * List all portfolios
+     */
+    public function index() : AnonymousResourceCollection;
+
+    /**
+     * Show Portfolio
+     */
+    public function show(Portfolio $portfolio) : PortfolioResource;
 }
--- app/Services/Services/PortfolioService.php
+++ app/Services/Services/PortfolioService.php
@@ -2,9 +2,28 @@
 
 namespace App\Services\Services;
 
+use App\Http\Resources\PortfolioResource;
+use App\Models\Portfolio;
 use App\Services\Constructors\PortfolioConstructor;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PortfolioService implements PortfolioConstructor
 {
-    //
+    /**
+     * List All Portfolios
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        return PortfolioResource::collection(
+            Portfolio::all()
+        );
+    }
+
+    /**
+     * Show Portfolio
+     */
+    public function show(Portfolio $portfolio): PortfolioResource
+    {
+        return PortfolioResource::make($portfolio);
+    }
 }
