be492bf6f805d24b7ac0539d485aed794af60199
csw <zobirofkir19@gmail.com>
2025-10-13 11:13:02 +0100

    create get-started

--- database/migrations/2025_10_13_100750_create_get_starteds_table.php
+++ database/migrations/2025_10_13_100750_create_get_starteds_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('get_starteds', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('company_name');
+            $table->string('phone_number');
+            $table->string('service');
+            $table->string('project_type');
+            $table->string('budget');
+            $table->string('project_timeline');
+            $table->longText('project_description');
             $table->timestamps();
         });
     }
