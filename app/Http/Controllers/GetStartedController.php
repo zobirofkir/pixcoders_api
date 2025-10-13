a9ad4d12b658aaf0b302670ecfdc6ce73de08d04
csw <zobirofkir19@gmail.com>
2025-10-13 11:20:01 +0100

    create get started controller

--- app/Http/Controllers/GetStartedController.php
+++ app/Http/Controllers/GetStartedController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\GetStartedRequest;
+use App\Http\Resources\GetStartedResource;
+use App\Models\GetStarted;
 use Illuminate\Http\Request;
 
 class GetStartedController extends Controller
 {
-    //
+    /**
+     * Get Started
+     */
+    public function store(GetStartedRequest $request) : GetStartedResource
+    {
+        return GetStartedResource::make(
+            GetStarted::create($request->validated())
+        );
+    }
 }
