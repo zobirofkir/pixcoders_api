932801360f3a458ad2bdc59bfa76d5faedb7ed35
csw <zobirofkir19@gmail.com>
2025-10-13 11:17:59 +0100

    create get-started request

--- app/Http/Requests/GetStartedRequest.php
+++ app/Http/Requests/GetStartedRequest.php
@@ -11,7 +11,7 @@ class GetStartedRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,30 @@ class GetStartedRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'company_name' => 'nullable|string|max:255',
+            'phone_number' => 'nullable|string|max:30',
+            'service' => 'required|string|max:255',
+            'project_type' => 'nullable|string|max:255',
+            'budget' => 'nullable|string|max:255',
+            'project_timeline' => 'nullable|string|max:255',
+            'project_description' => 'required|string|min:10',
+        ];
+    }
+
+    /**
+     * Get custom error messages for validation rules.
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Your name is required.',
+            'email.required' => 'An email address is required.',
+            'email.email' => 'Please enter a valid email address.',
+            'service.required' => 'Please select a service.',
+            'project_description.required' => 'Please provide a short description of your project.',
+            'project_description.min' => 'The project description must be at least 10 characters long.',
         ];
     }
 }
