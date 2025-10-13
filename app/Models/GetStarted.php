b8fa615ed965ea960b4077d913e73a91d307424e
csw <zobirofkir19@gmail.com>
2025-10-13 11:14:26 +0100

    create get started

--- app/Models/GetStarted.php
+++ app/Models/GetStarted.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class GetStarted extends Model
 {
-    //
+
+    /**
+     * Define Fillables
+     */
+    protected $fillable = [
+        "name",
+        "email",
+        "company_name",
+        "phone_number",
+        "service",
+        "project_type",
+        "budget",
+        "project_timeline",
+        "project_description",
+    ];
+    
 }
