62bce5392bc800748e0e12e6019ba45461b8c7ad
csw <zobirofkir19@gmail.com>
2025-10-13 11:18:55 +0100

    create get started resource

--- app/Http/Resources/GetStartedResource.php
+++ app/Http/Resources/GetStartedResource.php
@@ -14,6 +14,19 @@ class GetStartedResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'company_name' => $this->company_name,
+            'phone_number' => $this->phone_number,
+            'service' => $this->service,
+            'project_type' => $this->project_type,
+            'budget' => $this->budget,
+            'project_timeline' => $this->project_timeline,
+            'project_description' => $this->project_description,
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
+        ];
     }
 }
