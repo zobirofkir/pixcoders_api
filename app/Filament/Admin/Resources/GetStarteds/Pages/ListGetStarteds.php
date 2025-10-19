e16cf6cf063f488f716dfbf64a4547657fad1528
csw <zobirofkir19@gmail.com>
2025-10-20 00:55:00 +0100

    remove create

--- app/Filament/Admin/Resources/GetStarteds/Pages/ListGetStarteds.php
+++ app/Filament/Admin/Resources/GetStarteds/Pages/ListGetStarteds.php
@@ -13,7 +13,7 @@ class ListGetStarteds extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            // CreateAction::make(),
         ];
     }
 }
