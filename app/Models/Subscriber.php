db58fb33020d4f7ba54ffc4b13374347c430f34c
csw <zobirofkir19@gmail.com>
2025-11-04 12:49:52 +0100

    update

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -3,11 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Permission\Traits\HasRoles;
 
 class Subscriber extends Model
 {
-    use HasRoles;
     protected $fillable = [
         "name",
         "phone",
