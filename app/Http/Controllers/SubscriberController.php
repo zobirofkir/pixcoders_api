2f6e8a5a379223c176b95f266d95b1984dc57e93
csw <zobirofkir19@gmail.com>
2025-11-04 11:35:46 +0100

    update

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SubscriberRequest;
+use App\Http\Resources\SubscriberResource;
+use App\Models\Subscriber;
 use Illuminate\Http\Request;
 
 class SubscriberController extends Controller
 {
-    //
+    /**
+     * Store The Subscribtion
+     */
+    public function store(SubscriberRequest $request) : SubscriberResource
+    {
+        return SubscriberResource::make(
+            Subscriber::create(
+                $request->validated()
+            )
+        );
+    }
 }
