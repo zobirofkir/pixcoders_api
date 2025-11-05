9b0d80e7500aa39470030e715501ff948b2fbaec
csw <zobirofkir19@gmail.com>
2025-11-05 07:47:49 +0100

    add slug in portfolio

--- app/Observers/PortfolioObserver.php
+++ app/Observers/PortfolioObserver.php
@@ -2,7 +2,47 @@
 
 namespace App\Observers;
 
+use App\Models\Portfolio;
+
 class PortfolioObserver
 {
-    //
+    /**
+     * Handle the Portfolio "created" event.
+     */
+    public function created(Portfolio $portfolio): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Portfolio "updated" event.
+     */
+    public function updated(Portfolio $portfolio): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Portfolio "deleted" event.
+     */
+    public function deleted(Portfolio $portfolio): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Portfolio "restored" event.
+     */
+    public function restored(Portfolio $portfolio): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Portfolio "force deleted" event.
+     */
+    public function forceDeleted(Portfolio $portfolio): void
+    {
+        //
+    }
 }
